caa7ace0202671c8bb45eb90b841a15ab05575e0
Sidney Silva Braz de Oliveira <silvasidney721@gmail.com>
2024-02-20 17:35:19 +0100

    deploy: home page added

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Hana from './assets/hana_logo.png'
+
+function App() {
+
+  return (
+    <>
+    <a href="https://github.com/gethana/app">
+      <img src={Hana} alt="Hana logo" />
+    </a>
+      <p>
+        Download the Hana app from the <a href="https://github.com/gethana/app" target="_blank" rel="noreferrer">GitHub repository</a>.
+      </p>
+    </>
+  )
+}
+
+export default App
