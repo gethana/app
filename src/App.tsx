e1761a1098581aa0f8c296ad198c4e0b2c8b63a8
Sidney Silva Braz de Oliveira <silvasidney721@gmail.com>
2024-02-26 21:16:05 +0100

    feat: moved app to Tauri.app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
-import { Route, Switch } from "wouter";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Error404 from "./pages/Error404";
-import Register from "./pages/Register";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
 
 function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
   return (
-    <>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route component={Error404}></Route>
+    <div className="container">
+      <h1>Welcome to Tauri papapapapa</h1>
 
-      </Switch>
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
 
-    </>
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
   );
 }
 
